3a240499aaacf918561ca3e2d118ac5b1f11984e
shakyapramod <75612129+shakyapramod@users.noreply.github.com>
2020-12-10 14:31:40 +0530

    Update UtilityTests.cs

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -8,10 +8,11 @@ namespace UnitTests
         [Fact]
         public void TestLoggingEvents()
         {
-            Assert.Equal(1000, LoggingEvents.Healthcheck);
-            Assert.Equal(2001, LoggingEvents.GetAllPOIs);
-            Assert.Equal(2002, LoggingEvents.GetPOIByID);
-            Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
+           // Assert.Equal(1000, LoggingEvents.Healthcheck);
+           // Assert.Equal(2001, LoggingEvents.GetAllPOIs);
+            //Assert.Equal(2002, LoggingEvents.GetPOIByID);
+            //Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
+            Assert.Equal(1,2);
         }
     }
 }
